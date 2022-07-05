dc66ba9c7f847b1fccd9ec632110f2c9b9f2ab0a
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-07-05 19:11:32 -0400

    Time: 228 ms (26.55%), Space: 50.2 MB (60.18%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.ts
@@ -0,0 +1,30 @@
+class ParkingSystem {
+    big: number;
+    medium: number;
+    small: number;
+    constructor(big: number, medium: number, small: number) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    addCar(carType: number): boolean {
+      if (carType === 1 && this.big > 0) {
+          this.big-- 
+          return true;
+      } else if (carType === 2 && this.medium > 0) {
+          this.medium--
+          return true;
+      } else if (carType === 3 && this.small > 0) {
+          this.small--
+          return true;
+      }
+        return false
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
