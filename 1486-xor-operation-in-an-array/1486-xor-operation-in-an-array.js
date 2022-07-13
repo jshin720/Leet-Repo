ec9fdd712fb0af5911eb3afb4267ea03c9f37295
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-07-13 16:21:56 -0400

    Time: 74 ms (69.92%), Space: 42 MB (38.14%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.js
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.js
@@ -9,10 +9,9 @@ var xorOperation = function(n, start) {
     for (let i = 1; i < n; i++) {
         numsArr.push(start + 2 * i) ;
     }
-    console.log(numsArr)
- 
+    
    return numsArr.reduce((a, b) => 
-        // console.log(a, b)
+       
          a ^ b 
     )
     
