9033d091b87b0573b8ffae169810666dbb022ca8
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-07-14 17:40:17 -0400

    Time: 139 ms (74.14%), Space: 57.3 MB (67.81%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -3,14 +3,20 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    let common = 0;
+//     let common = 0;
     
-    for (let i = 0; i < edges.length; i++ ) {
-        if (edges[i].includes(edges[0][0])){
-            common = edges[0][0];
-        } else if ( edges[i].includes(edges[0][1])) {
-            common = edges[0][1];
-        }
+//     for (let i = 0; i < edges.length; i++ ) {
+//         if (edges[i].includes(edges[0][0])){
+//             common = edges[0][0];
+//         } else if ( edges[i].includes(edges[0][1])) {
+//             common = edges[0][1];
+//         }
+//     }
+//     return common; 
+    
+    if (edges[0].includes(edges[1][0])) {
+        return edges[1][0];
+    } else if (edges[0].includes(edges[1][1])) {
+        return edges[1][1]
     }
-    return common; 
 };
