4786da2213aaa75ffb5e451b8418108610f79d40
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-07-27 17:59:24 -0400

    Time: 101 ms (39.82%), Space: 42.1 MB (61.95%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.js
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.js
@@ -4,23 +4,34 @@
  // * @return {number}
  // */
 var minBitFlips = function(start, goal) {
-    let sBinary = start.toString(2);
-    let gBinary = goal.toString(2);
-    let count = 0;
+//     let sBinary = start.toString(2);
+//     let gBinary = goal.toString(2);
+//     let count = 0;
  
-    if (sBinary.length > gBinary.length) {
-        gBinary = gBinary.padStart(sBinary.length, "0")
-    }
+//     if (sBinary.length > gBinary.length) {
+//         gBinary = gBinary.padStart(sBinary.length, "0")
+//     }
     
-    if (gBinary.length > sBinary.length) {
-        sBinary = sBinary.padStart(gBinary.length, "0")
-    }
+//     if (gBinary.length > sBinary.length) {
+//         sBinary = sBinary.padStart(gBinary.length, "0")
+//     }
     
     
-    for (let i = sBinary.length - 1; i >= 0; i--) {
-         console.log(sBinary[i])
-         console.log(gBinary[i])
-       gBinary[i] !== sBinary[i] ? count++ : count; 
+//     for (let i = sBinary.length - 1; i >= 0; i--) {
+//          console.log(sBinary[i])
+//          console.log(gBinary[i])
+//        gBinary[i] !== sBinary[i] ? count++ : count; 
+//     }
+//     return count; 
+    let binaryNum = start ^ goal;
+    let count = 0
+    
+    while(binaryNum){
+        count += binaryNum & 1;
+        binaryNum >>>= 1;
+       // console.log(binaryNum)
+      
     }
-    return count; 
+   
+    return count;
 };
