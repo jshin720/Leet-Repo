b2ad61efa68ebc9a49425776e2e3646109271410
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-07-28 15:01:43 -0400

    Time: 81 ms (70.52%), Space: 42.7 MB (82.47%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.js
+++ 804-unique-morse-code-words/804-unique-morse-code-words.js
@@ -3,32 +3,47 @@
  * @return {number}
  */
 var uniqueMorseRepresentations = function(words) {
-    const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",
-                   ".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-    const alpha = ["a","b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",                 "t", "u", "v", "w", "x", "y", "z"];
-    let count = [];
-    let morseKey = {};
+//     const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",
+//                    ".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+//     const alpha = ["a","b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",                 "t", "u", "v", "w", "x", "y", "z"];
+//     let count = [];
+//     let morseKey = {};
     
-    morse.forEach((morseAlpha, i) => {
-        morseKey[alpha[i]] = morseAlpha;
-        return morseKey;
-    })
+//     morse.forEach((morseAlpha, i) => {
+//         morseKey[alpha[i]] = morseAlpha;
+//         return morseKey;
+//     })
 
-    const morsedWords = words.map((word) => {
-         let newWord = "";
+//     const morsedWords = words.map((word) => {
+//          let newWord = "";
                              
-        for (let i = 0; i < word.length; i++) {
-            newWord += morseKey[word[i]];
-        }
+//         for (let i = 0; i < word.length; i++) {
+//             newWord += morseKey[word[i]];
+//         }
         
-        return newWord;
-    })
+//         return newWord;
+//     })
     
-    while(morsedWords.length > 0) {
-        let checkingWord = morsedWords.shift();
-        if(!count.includes(checkingWord)) count.push(checkingWord);
-        console.log(morsedWords)
-    }
+//     while(morsedWords.length > 0) {
+//         let checkingWord = morsedWords.shift();
+//         if(!count.includes(checkingWord)) count.push(checkingWord);
+//         console.log(morsedWords)
+//     }
     
-    return count.length; 
+//     return count.length; 
+    
+    let morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+	let map = new Set()
+
+	words.forEach(word => {
+		let s = ""
+		for(let i = 0; i < word.length; i++) {
+			s += morseCode[word[i].charCodeAt(0) - 97]
+		}
+
+		map.add(s)
+	})
+
+	return map.size
 };
