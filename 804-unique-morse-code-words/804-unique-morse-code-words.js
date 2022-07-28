b2553b7f5c901db3b33448fb6093cd23cd91fac1
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-07-28 13:11:43 -0400

    Time: 313 ms (5.59%), Space: 50.4 MB (5.20%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+    const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",
+                   ".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+    const alpha = ["a","b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",                 "t", "u", "v", "w", "x", "y", "z"];
+    let count = [];
+    let morseKey = {};
+    
+    morse.forEach((morseAlpha, i) => {
+        morseKey[alpha[i]] = morseAlpha;
+        return morseKey;
+    })
+
+    const morsedWords = words.map((word) => {
+         let newWord = "";
+                             
+        for (let i = 0; i < word.length; i++) {
+            newWord += morseKey[word[i]];
+        }
+        
+        return newWord;
+    })
+    
+    while(morsedWords.length > 0) {
+        let checkingWord = morsedWords.shift();
+        if(!count.includes(checkingWord)) count.push(checkingWord);
+        console.log(morsedWords)
+    }
+    
+    return count.length; 
+};
