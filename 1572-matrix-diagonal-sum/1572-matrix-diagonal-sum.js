776da6ccabd393371cd23a43aabdc7d4d755e936
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-02 12:51:20 -0400

    Time: 70 ms (87.88%), Space: 42.9 MB (88.29%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -5,22 +5,32 @@
 var diagonalSum = function(mat) {
     let sumArr = [];
     let primaryD = 0;
-    let secondaryD = mat[0].length - 1;
-     // console.log("before", primaryD, secondaryD);
+//     let secondaryD = mat[0].length - 1;
+//      // console.log("before", primaryD, secondaryD);
     
     
-    for (let i = 0; i < mat.length; i++) {
-       // console.log(!sumArr.includes(mat[i][primaryD]), !sumArr.includes(mat[i][secondaryD]))
+//     for (let i = 0; i < mat.length; i++) {
+//        // console.log(!sumArr.includes(mat[i][primaryD]), !sumArr.includes(mat[i][secondaryD]))
         
-        if (primaryD !== secondaryD) {
-            sumArr.push(mat[i][primaryD])
-            sumArr.push(mat[i][secondaryD])
-        } else {
-            sumArr.push(mat[i][primaryD])
-        }
-        primaryD++;
-        secondaryD--;
-        // console.log("after", primaryD, secondaryD);
+//         if (primaryD !== secondaryD) {
+//             sumArr.push(mat[i][primaryD])
+//             sumArr.push(mat[i][secondaryD])
+//         } else {
+//             sumArr.push(mat[i][primaryD])
+//         }
+//         primaryD++;
+//         secondaryD--;
+//         // console.log("after", primaryD, secondaryD);
+//     }
+//     return sumArr.reduce((a, b) => a + b)
+      let sum = 0 ;
+    for(let i = 0;i<mat.length;i++){
+      sum+= mat[i][i];
     }
-    return sumArr.reduce((a, b) => a + b)
+    for(let i = 0;i<mat.length;i++){
+        if(i != mat.length-1-i ){
+            sum+= mat[i][mat.length-1-i];
+        }
+    }
+    return sum 
 };
