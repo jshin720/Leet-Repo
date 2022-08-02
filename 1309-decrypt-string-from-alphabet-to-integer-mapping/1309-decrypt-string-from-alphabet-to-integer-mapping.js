4a18745a7dbfee8298efd1d5cc61bedcb7e19ed8
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-02 14:16:34 -0400

    Time: 90 ms (53.25%), Space: 41.3 MB (99.11%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
@@ -4,22 +4,7 @@
  */
 var freqAlphabets = function(s) {
     let mapped = [];
-//     let premapped = s.split("");
-    
-    
-//     console.log(premapped)
-//     while(premapped) {
-//          let end = premapped.length - 1;
-//          if (premapped[premapped.end] === "#") {
-//             mapped.unshift(String.fromCharCode(Number(premapped[end - 2] + premapped[end - 1]) + 96))
-//             premapped.slice(0, premapped[end - 2])
-//         } else {
-//             mapped.unshift(String.fromCharCode(Number(premapped[end]) + 96))
-//             premapped.slice(0, premapped[end])
-//         }
-//     }
-        
-    // }
+
     for (let i = s.length - 1; i >= 0; i--) {
         if (s[i] === "#") {
             mapped.unshift(String.fromCharCode(Number(s[i - 2] + s[i - 1]) + 96))
@@ -28,6 +13,6 @@ var freqAlphabets = function(s) {
         } 
         mapped.unshift(String.fromCharCode(Number(s[i]) + 96))
     }
-    console.log(mapped)
+    
     return mapped.join("")
 };
