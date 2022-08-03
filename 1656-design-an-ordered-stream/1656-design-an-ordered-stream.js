40a6ae63c6e99549ad07dd6516c98f56341c0251
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-03 12:50:04 -0400

    Time: 294 ms (40.44%), Space: 54 MB (66.54%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
@@ -2,9 +2,10 @@
  * @param {number} n
  */
 var OrderedStream = function(n) {
-    this.pointer = 1;
-    this.temp = {};
-    
+    // this.pointer = 1;
+    // this.temp = {};
+    this.pointer = 0;
+    this.holder = new Array(n);
 };
 
 /** 
@@ -14,18 +15,27 @@ var OrderedStream = function(n) {
  */
 OrderedStream.prototype.insert = function(idKey, value) {
     let chunk = [];
-    // console.log(this.temp)
-    this.temp[idKey] = value; // inserts the idkey and value into the obj
-       // console.log(chunk)      
-    while(this.temp[this.pointer]) { // while this.temp[this.pointer] === defined ---- will not run if it is undefined
-        chunk.push(this.temp[this.pointer]); // pushes in the value into chunk arr
-        this.pointer++;
-    }
+//     // console.log(this.temp)
+//     this.temp[idKey] = value; // inserts the idkey and value into the obj
+//        // console.log(chunk)      
+//     while(this.temp[this.pointer]) { // while this.temp[this.pointer] === defined ---- will not run if it is undefined
+//         chunk.push(this.temp[this.pointer]); // pushes in the value into chunk arr
+//         this.pointer++;
+//     }
 
- // console.log(chunk)    
-// if the loop does run then returns an empty arr
+//  // console.log(chunk)    
+// // if the loop does run then returns an empty arr
+//     return chunk;
+// // reason for null is because pointer starts at 1. and second element an [] because of line 27
+    this.holder[idKey - 1] = value;
+    
+ 
+        while(this.holder[this.pointer]) {
+            chunk.push(this.holder[this.pointer]);
+            this.pointer++;
+        };
+    
     return chunk;
-// reason for null is because pointer starts at 1. and second element an [] because of line 27
 };
 /** 
  * Your OrderedStream object will be instantiated and called as such:
