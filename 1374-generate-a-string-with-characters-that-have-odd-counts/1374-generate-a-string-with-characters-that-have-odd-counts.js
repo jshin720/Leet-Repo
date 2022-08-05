8ccde15b69ae0540c1277bec8457e2455633a7ce
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-05 15:36:02 -0400

    Time: 83 ms (56.00%), Space: 42.1 MB (70.00%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -3,22 +3,22 @@
  * @return {string}
  */
 var generateTheString = function(n) {
-   let str = "";
-    let i = 1;
+//    let str = "";
+//     let i = 1;
     
-    if(n % 2 === 0) {
-        while(i <= n - 1) {
-            str += "u"
-            i++;
-        }   
-        str += "k"
-   }  else {
-      while(i <= n) {
-            str += "u"
-            i++;
-        }   
-   }
+//     if(n % 2 === 0) {
+//         while(i <= n - 1) {
+//             str += "u"
+//             i++;
+//         }   
+//         str += "k"
+//    }  else {
+//       while(i <= n) {
+//             str += "u"
+//             i++;
+//         }   
+//    }
+//     return str;
     
-    
-    return str;
+    return n % 2 === 0 ? "t".repeat(n - 1) + "p"  : "s".repeat(n)
 };
