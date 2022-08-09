c8707deaa1858a63c08a0f42a0651b7ec0fa68ca
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-09 13:52:36 -0400

    Time: 101 ms (51.75%), Space: 43.5 MB (51.32%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -17,7 +17,7 @@ var halvesAreAlike = function(s) {
     const mid = Math.floor(s.length / 2);
     let partA = s.slice(0, mid);
     let partB = s.slice(mid);
-    console.log(partA, partB)
+    
     if (partA.length !== partB.length) return false;
     
     if(vowelCount(partA) !== vowelCount(partB)) return false;
