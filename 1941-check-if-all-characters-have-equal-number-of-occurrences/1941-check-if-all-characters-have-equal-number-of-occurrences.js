392bb6ecd77f703670789b2f5993e896bd71fc87
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-11 15:09:41 -0400

    Time: 101 ms (58.13%), Space: 43.9 MB (70.93%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.js
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.js
@@ -6,8 +6,13 @@ var areOccurrencesEqual = function(s) {
     const occur = {};
     
     for (let i = 0; i < s.length; i++) {
-        !occur[s[i]] ? occur[s[i]] = 1 : occur[s[i]]++;
-    }
+        if (!occur[s[i]]) {
+             occur[s[i]] = 1 ;
+        } else {
+            occur[s[i]]++;
+        } ;
+    };
+    
     for (let key in occur) {
         if (occur[s[0]] !== occur[key]) return false;
     }
