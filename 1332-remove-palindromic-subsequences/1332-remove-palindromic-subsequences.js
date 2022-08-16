a29e0dd7d42fc1e6723cc1b50a7fbab62ca85e82
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-16 17:50:02 -0400

    Time: 90 ms (39.73%), Space: 42 MB (46.58%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
@@ -3,16 +3,17 @@
  * @return {number}
  */
 var removePalindromeSub = function(s) {
-    s = s.split("");
+//     s = s.split("");
   
     
-    if (s.join("") === s.reverse().join("")) {
-        return 1;
-    } else if (s.slice(1).join("") === s.slice(1).reverse().join("") ) {
-        return 1 + removePalindromeSub(s.slice(1).join(""));
-    } else if (s.slice(0, -1).join("") === s.slice(0, -1).reverse().join("")) {
-        return removePalindromeSub(s.slice(0, -1).join("")) + 1;
-    } else {
-        return 2;
-    }
+//     if (s.join("") === s.reverse().join("")) {
+//         return 1;
+//     } else if (s.slice(1).join("") === s.slice(1).reverse().join("") ) {
+//         return 1 + removePalindromeSub(s.slice(1).join(""));
+//     } else if (s.slice(0, -1).join("") === s.slice(0, -1).reverse().join("")) {
+//         return removePalindromeSub(s.slice(0, -1).join("")) + 1;
+//     } else {
+//         return 2;
+//     }
+      return !s.length ? 0 : s === s.split("").reverse().join('') ? 1 : 2
 };
