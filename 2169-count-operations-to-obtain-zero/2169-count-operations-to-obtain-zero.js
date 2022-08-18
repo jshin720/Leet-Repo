bc361c989689c23159f181bc32ff70ae6aa81896
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-18 13:35:24 -0400

    Time: 128 ms (8.68%), Space: 42.1 MB (57.99%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.js
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.js
@@ -6,8 +6,7 @@
 var countOperations = function(num1, num2) {
     let count = 0;
      
-    while (num1 > 0 && num2 > 0) {
-       
+    while (num1 * num2 > 0) {
         if ( num1 >= num2) {
             num1 -= num2;
         } else {
