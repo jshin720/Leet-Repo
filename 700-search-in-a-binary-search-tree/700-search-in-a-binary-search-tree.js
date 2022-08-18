56c6b8aa6fba19fba4c4c72c3923a33363c5d1d8
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-18 14:12:02 -0400

    Time: 338 ms (5.28%), Space: 53 MB (8.42%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    let queue = [root];
+    
+    while (queue.length) {
+       let node =  queue.pop();
+        console.log(node.left)
+        if (node.val === val) return node;
+        if (node.left) queue.push(node.left)
+        if (node.right) queue.push(node.right)
+        
+    }
+    return null;
+};
