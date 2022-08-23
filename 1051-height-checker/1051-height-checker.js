4293c02d2a3ef24ceaea9ec5257c04e7d4f3b5a4
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-23 14:45:06 -0400

    Time: 75 ms (80.88%), Space: 41.6 MB (96.35%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -7,7 +7,7 @@ var heightChecker = function(heights) {
     let count = 0;
     
     for (let i = 0; i < heights.length; i++) {
-        console.log(heights[i], expected[i])
+        
         heights[i] !== expected[i] && count++;
     }
     
