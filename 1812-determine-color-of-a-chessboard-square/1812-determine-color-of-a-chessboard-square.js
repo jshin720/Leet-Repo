45e2e6d706c59750df0b69346ed1cca865d25d89
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-23 13:31:15 -0400

    Time: 125 ms (5.43%), Space: 41.9 MB (60.47%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
@@ -3,5 +3,5 @@
  * @return {boolean}
  */
 var squareIsWhite = function(coordinates) {
-    return ((coordinates.charCodeAt(0) - 96) + Number(coordinates[1])) % 2 !== 0;
+    return ((coordinates[0].charCodeAt() - 96) + Number(coordinates[1])) % 2 !== 0;
 };
