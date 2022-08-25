7175fac2acdfb2afc8b0a555897dec219e78577c
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-25 11:42:01 -0400

    Time: 93 ms (40.26%), Space: 42.3 MB (25.97%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+    let count = 0;
+    
+    for (let char of s) {
+        char === letter && count++;
+    }
+    
+    return Math.floor((count / s.length) * 100);
+};
