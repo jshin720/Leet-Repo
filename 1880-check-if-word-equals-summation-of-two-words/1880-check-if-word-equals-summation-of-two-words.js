f54bf0c490748f001265fd24096f8f916096e13f
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-25 12:13:58 -0400

    Time: 142 ms (5.40%), Space: 44 MB (6.08%) - LeetHub

---
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} firstWord
+ * @param {string} secondWord
+ * @param {string} targetWord
+ * @return {boolean}
+ */
+var isSumEqual = function(firstWord, secondWord, targetWord) {
+  
+    const wordSum = (word) => {
+        let sum = word.split("");
+        for (let i in word) {
+            sum[i] = word[i].charCodeAt() - 97;
+        }
+          console.log(sum)
+        return Number(sum.join(""));
+    }
+    
+    return wordSum(firstWord) + wordSum(secondWord) === wordSum(targetWord)
+};
