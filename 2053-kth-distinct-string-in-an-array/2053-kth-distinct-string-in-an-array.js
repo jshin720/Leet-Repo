db0ceca1bf1e13ce1eb48e1640dc36d9d1fdb635
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-26 15:08:31 -0400

    Time: 117 ms (51.56%), Space: 46.1 MB (33.22%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function(arr, k) {
+    const count = {};
+    
+    for (let char of arr) {
+        
+        if (!count[char]) {
+            count[char] = 1;
+        } else {
+            count[char]++;
+        }
+    }
+    
+    // console.log(count)
+    let distinct = [];
+    
+    for (let char in count) {
+        if (count[char] === 1) distinct.push(char);
+    }
+    
+    return !distinct[k - 1] ? "" : distinct[k - 1];
+}; 
