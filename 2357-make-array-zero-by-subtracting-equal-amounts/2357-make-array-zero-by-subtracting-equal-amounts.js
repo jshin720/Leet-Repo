4efef86308e3bd95d8f75e62ccfa392d43cfbc6a
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-27 11:07:43 -0400

    Time: 95 ms (49.25%), Space: 44.1 MB (22.13%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+    let count = 0;
+    
+   while (!nums.every((num) => num === 0)) {
+       let min = Math.min(...nums.filter((num) => num !== 0) );
+     
+       for (let i in nums) {
+      
+           if (nums[i] !== 0) nums[i] -= min;
+       }
+      
+       count++;
+       
+   }
+    return count;
+};
