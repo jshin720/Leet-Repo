7b2e247b02ed82e2f510266ea5add88a7704f90d
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-27 18:09:33 -0400

    Time: 58 ms (98.52%), Space: 42 MB (82.12%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -3,18 +3,20 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
-    let count = 0;
+//     let count = 0;
     
-   while (!nums.every((num) => num === 0)) {
-       let min = Math.min(...nums.filter((num) => num !== 0) );
+//    while (!nums.every((num) => num === 0)) {
+//        let min = Math.min(...nums.filter((num) => num !== 0) );
      
-       for (let i in nums) {
+//        for (let i in nums) {
       
-           if (nums[i] !== 0) nums[i] -= min;
-       }
+//            if (nums[i] !== 0) nums[i] -= min;
+//        }
       
-       count++;
+//        count++;
        
-   }
-    return count;
+//    }
+//     return count;
+    
+    return new Set(nums.filter((num) => num !== 0)).size;
 };
