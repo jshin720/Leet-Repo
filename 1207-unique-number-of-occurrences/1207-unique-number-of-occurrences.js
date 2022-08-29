6de6fc2d22a447e7f0a0599752c4a87b1fb4c577
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-29 14:29:35 -0400

    Time: 106 ms (23.60%), Space: 42.4 MB (77.47%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,23 +3,30 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    const map = {};
-    const unique = new Set()
+//     const map = {};
+//     const unique = new Set()
     
-    for (let num of arr) {
-        if (!map[num]) {
-            map[num] = 1;
-        } else {
-            map[num]++;
-        }
-    }
+//     for (let num of arr) {
+//         if (!map[num]) {
+//             map[num] = 1;
+//         } else {
+//             map[num]++;
+//         }
+//     }
     
-    for (let count in map) {
-        if (!unique.has(map[count])) {
-            unique.add(map[count])
-        } else {
-            return false;
-        }
-    }
-    return true; 
+//     for (let count in map) {
+//         if (!unique.has(map[count])) {
+//             unique.add(map[count])
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true; 
+    let obj = arr.reduce((a,c) => {
+    a[c] = a[c] + 1 || 1
+    return a
+},{})
+
+let values = Object.values(obj)
+return values.length  == new Set(values).size
 };
