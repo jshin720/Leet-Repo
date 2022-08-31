052e9e3459509d66a2418aefc5ab027ea71f9ebe
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-08-31 18:04:19 -0400

    Time: 64 ms (97.60%), Space: 43.1 MB (43.84%) - LeetHub

---
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {number}
+ */
+var countPrefixes = function(words, s) {
+    let count = 0;
+    
+    for (let word of words) {
+        s.startsWith(word) && count++;
+    }
+    return count; 
+};
