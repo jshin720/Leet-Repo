130687a4fe78a8ca2a9c1a0738debe232928d326
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-07 18:55:02 -0400

    Time: 202 ms (21.10%), Space: 49.8 MB (46.20%) - LeetHub

---
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    let indices = {};
+    
+    for (let i in list1) {
+        let idx = list2.indexOf(list1[i]);
+        if (idx !== -1) indices[list1[i]] = Number(i) + idx;
+    }
+    
+    const min =  Math.min(...Object.values(indices));
+    
+    
+    let minIndex = [];
+    
+    for (let word in indices) {
+        if (indices[word] === min) minIndex.push(word);
+    }
+    
+    return minIndex;
+};
+
