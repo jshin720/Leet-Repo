324e4db037c5024e1cc79116241061adb2f9340a
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-12 18:12:49 -0400

    Time: 152 ms (14.36%), Space: 45.1 MB (80.22%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.js
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.js
@@ -11,36 +11,32 @@
  * @return {number[]}
  */
 var postorder = function(root) {
-   
-//     const posted = [];
-//     const stack = [root];
-   
-//     while (stack) {
-//         let node = stack.pop();
-//         if (!node) return;
-        
-
-//         // console.log(node);
-//         posted.push(node.val);
-//         for (let i = 0; i < node.children.length; i++) {
-//             stack.push(node.children[i]);
-//         }
-        
-//       console.log(posted)
-//     }
-    
-    
-//     return posted; 
-    
-    const res = [];
-    
-    function post(node) {
+   if (!root) return [];
+    const posted = [];
+    const stack = [root];
+  
+    while (stack.length !== 0) {
+        let node = stack.pop();
         if (!node) return;
-        for (let child of node.children) {
-            post(child);
+        
+        posted.push(node.val);
+        for (let i = 0; i < node.children.length; i++) {
+            stack.push(node.children[i]);
         }
-        res.push(node.val);
     }
-    post(root);
-    return res;
+    
+    
+    return posted.reverse(); 
+    
+//     const res = [];
+    
+//     function post(node) {
+//         if (!node) return;
+//         for (let child of node.children) {
+//             post(child);
+//         }
+//         res.push(node.val);
+//     }
+//     post(root);
+//     return res;
 };
