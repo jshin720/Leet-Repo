cb1146aaa8df43351eeb3cf2387c8bbfd5e1283c
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-13 12:47:52 -0400

    Time: 100 ms (28.37%), Space: 41.4 MB (92.91%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.js
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 var sumBase = function(n, k) {
-//     const converted = n.toString(k);
-//     let sum = 0;
-    
-//     for (const num of converted) {
-//         sum += +num;
-//     }
-    
-//     return sum;
-    
+    const converted = n.toString(k);
     let sum = 0;
-    while(n > 0){
-        sum += n % k;
-        n = Math.floor(n/k);
+    
+    for (const num of converted) {
+        sum += +num;
     }
+    
     return sum;
+    
+    // let sum = 0;
+    // while(n > 0){
+    //     sum += n % k;
+    //     n = Math.floor(n/k);
+    // }
+    // return sum;
 };
