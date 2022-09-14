08427dbc9345b1933bc654b2fdb79752644b2eac
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-14 14:24:34 -0400

    Time: 137 ms (7.68%), Space: 45.5 MB (30.74%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -12,14 +12,14 @@
  */
 var maxDepth = function(root) {
     
-    const dfs = (node, depth = 0) => {
-        if (!node) return depth;
+//     const dfs = (node, depth = 0) => {
+//         if (!node) return depth;
         
-         return Math.max(dfs(node.left, depth + 1), dfs(node.right, depth + 1)); 
-    };
+//          return Math.max(dfs(node.left, depth + 1), dfs(node.right, depth + 1)); 
+//     };
     
-    return dfs(root);
+//     return dfs(root);
     
     
-
+    return root ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 : 0
 };
