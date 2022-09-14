c85f28f9b585c030fb7877444e3676dd1ac9f0b8
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-14 13:42:40 -0400

    Time: 63 ms (93.78%), Space: 41.9 MB (93.14%) - LeetHub

---
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    const reverse = (node) => {
+        if (!node) return;
+        
+        let left = node.left;
+        let right = node.right;
+        
+        node.left = right;
+        node.right = left;
+        
+        reverse(left);
+        reverse(right);
+        
+    }
+    
+    reverse(root);
+    
+    return root;
+};
