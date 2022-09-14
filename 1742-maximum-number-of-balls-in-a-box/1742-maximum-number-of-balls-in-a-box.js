c6ac516e5bf3ffc40c5cbbb6f5741d6a4ec769e7
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-14 12:29:56 -0400

    Time: 218 ms (25.43%), Space: 49.2 MB (31.79%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.js
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.js
@@ -5,14 +5,17 @@
  */
 var countBalls = function(lowLimit, highLimit) {
     const boxes = {};
-    let max = 0;
+    let max = 0
+    
     for (let balls = lowLimit; balls <= highLimit; balls++) {
         let sum = 0;
         const ballString = balls.toString(); 
+        
         for (let i = 0; i < ballString.length; i++) {
             sum += +ballString[i];
          
         };
+        
         !boxes[sum] ? boxes[sum] = 1 : boxes[sum]++;
         if (boxes[sum] > max) max = boxes[sum]
         sum = 0;
