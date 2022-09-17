274ec593fb4152a98b2bf41bf2cf0eea3e067368
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-17 00:53:55 -0400

    Time: 80 ms (89.19%), Space: 43 MB (35.15%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -6,9 +6,21 @@
 var twoSum = function(nums, target) {
     
     
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) return [i, j];
+    //     }
+    // }
+    
+    const mapped = {};
+    
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) return [i, j];
-        }
+        let targetNum = target - nums[i];
+        
+       if (mapped[targetNum] !== undefined) {
+            return [mapped[targetNum], i];
+       } else {
+           mapped[nums[i]] = i;
+       }
     }
 };
