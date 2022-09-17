213ecc0647a240568653d8c6658d8876b16ef537
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-17 01:06:10 -0400

    Time: 196 ms (7.11%), Space: 41.8 MB (57.46%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -6,15 +6,17 @@ var fib = function(n) {
    if(n === 0) return 0;
    if(n === 1) return 1;
     
-    const fibArr = [0, 1];
+    // const fibArr = [0, 1];
     
-    while (n > 1) {
-        let lastNum = fibArr[fibArr.length - 1];
-        let secNum = fibArr[fibArr.length - 2];
+//     while (n > 1) {
+//         let lastNum = fibArr[fibArr.length - 1];
+//         let secNum = fibArr[fibArr.length - 2];
         
-        fibArr.push(lastNum + secNum);
-        n--;
+//         fibArr.push(lastNum + secNum);
+//         n--;
         
-    }
-    return fibArr.pop()
+//     }
+//     return fibArr.pop()
+    
+    return fib(n -  1) + fib(n - 2);
 };
