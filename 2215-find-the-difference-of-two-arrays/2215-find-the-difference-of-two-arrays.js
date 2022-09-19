5f9b6b9d6a22e226263363681e61916d07544f7a
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-19 13:50:51 -0400

    Time: 170 ms (52.52%), Space: 49 MB (35.01%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -4,17 +4,25 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    const ansOne = [];
-    const ansTwo = [];
+//     const ansOne = [];
+//     const ansTwo = [];
     
-    for (let num of nums1) {
-        if (!nums2.includes(num) && !ansOne.includes(num)) ansOne.push(num);    
-    }
+//     for (let num of nums1) {
+//         if (!nums2.includes(num) && !ansOne.includes(num)) ansOne.push(num);    
+//     }
     
-     for (let num of nums2) {
-        if (!nums1.includes(num) && !ansTwo.includes(num)) ansTwo.push(num);    
-    }
+//      for (let num of nums2) {
+//         if (!nums1.includes(num) && !ansTwo.includes(num)) ansTwo.push(num);    
+//     }
     
+    
+    num1 = new Set(nums1);
+    num2 = new Set(nums2);
+    
+    const ansOne = [...num1].filter((num) => !num2.has(num));
+    const ansTwo = [...num2].filter((num) => !num1.has(num));
+
+    // console.log(ansOne, ansTwo)
     
     return [ansOne, ansTwo]
 };
