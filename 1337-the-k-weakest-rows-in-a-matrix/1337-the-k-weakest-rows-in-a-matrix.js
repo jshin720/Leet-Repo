6a59f656f907f82e88cc799651364082802f53b7
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-21 13:34:22 -0400

    Time: 111 ms (31.28%), Space: 43.8 MB (85.91%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -4,29 +4,44 @@
  * @return {number[]}
  */
 var kWeakestRows = function(mat, k) {
-    const weakest = {};
+//     const weakest = {};
+    
+//     for (let i = 0; i < mat.length; i++) {
+//         let countSol = 0
+        
+//         for (let j = 0; j < mat[i].length; j++) {
+//             mat[i][j] === 1 && countSol++ 
+//         }
+        
+//         weakest[i] = countSol;
+//         countSol = 0;
+      
+//     }
+    
+//     const sorted =  Object.entries(weakest).sort((a ,b) => a[1] - b[1] )
+
+//     const ans = [];
+//     let i = 0;
+//     while (ans.length < k) {
+    
+//         ans.push(sorted[i][0])
+//         i++;
+//     }
+    
+//     return ans;
+    
+    const map = {}, ans = [];
     
     for (let i = 0; i < mat.length; i++) {
-        let countSol = 0
+        map[i] = 0;
         
         for (let j = 0; j < mat[i].length; j++) {
-            mat[i][j] === 1 && countSol++ 
+            mat[i][j] === 1 && map[i]++;
         }
         
-        weakest[i] = countSol;
-        countSol = 0;
-      
+        ans.push(i);
     }
     
-    const sorted =  Object.entries(weakest).sort((a ,b) => a[1] - b[1] )
-
-    const ans = [];
-    let i = 0;
-    while (ans.length < k) {
-    
-        ans.push(sorted[i][0])
-        i++;
-    }
-    
-    return ans;
+    ans.sort((a, b) => map[a] - map[b]);
+    return ans.slice(0, k)
 };
