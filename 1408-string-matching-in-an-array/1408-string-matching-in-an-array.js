5798a573c3ea840cdde49fb4304bde967ccc1c6b
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-22 11:29:45 -0400

    Time: 110 ms (38.29%), Space: 42.4 MB (66.17%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.js
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.js
@@ -3,14 +3,32 @@
  * @return {string[]}
  */
 var stringMatching = function(words) {
-//     const ans = [];
+    const ans = [];
     
-//     for (let i = 0; i < words.length; i++) {
-//         for (let j = i + 1; j < words.length; j++){
-//             if(words[i].includes(words[j])) ans.push(words[j])
-//             break;
-//         }
-//     }
-//     console.log(ans)
-    return words.filter(n => words.some(h => h !== n && h.includes(n)));
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words.length; j++){
+           if (words[i] !== words[j])
+           if (words[j].includes(words[i]))
+            if(!ans.includes(words[i]))
+               ans.push(words[i])
+        }
+    }
+    
+    // words.filter((n) => {
+    //     console.log("n", n)
+    //     return words.some((h) => {
+    //        return  h !== n && h.includes(n);
+    //      )}
+    //  )}
+                 
+//    return words.filter((n) => {
+//         console.log("n", n);
+        
+//         return words.some((h) => {
+//             console.log("h", h)
+//            return h !== n && h.includes(n); 
+//         })
+//     })
+    return ans
+        
 };
