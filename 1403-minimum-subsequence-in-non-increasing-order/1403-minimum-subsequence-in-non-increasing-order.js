75d055af9d53e54ee35c9b3fb6bf2e0a66d2a461
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-22 12:26:18 -0400

    Time: 130 ms (29.31%), Space: 44.5 MB (57.76%) - LeetHub

--- 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.js
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.js
@@ -3,17 +3,18 @@
  * @return {number[]}
  */
 var minSubsequence = function(nums) {
-    const halfSums = nums.reduce((a, b) => a + b, 0) / 2;
+    let total = nums.reduce((a, b) => a + b, 0);
     nums.sort((a, b) => b - a);
    
     const res = [];
+    
     let sum = 0;
   
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
         res.push(nums[i]);
        
-        if (sum > halfSums) return res; 
+        if (sum > (total- sum)) return res; 
     }
    
     
