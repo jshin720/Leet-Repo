0c0e746c00410ab272905f3058aa126b72a9fd13
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-23 16:39:57 -0400

    Time: 103 ms (53.38%), Space: 44.5 MB (68.24%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
@@ -4,28 +4,47 @@
  */
 var luckyNumbers  = function(matrix) {
           
-    const rowMin = (matrix) => {
-        const min = new Set()
-        for (let arr of matrix) {
-            min.add(Math.min(...arr))
-        } 
-        return min;
-    }
+//     const rowMin = (matrix) => {
+//         const min = new Set()
+//         for (let arr of matrix) {
+//             min.add(Math.min(...arr))
+//         } 
+//         return min;
+//     }
     
-    const min = rowMin(matrix);
+//     const min = rowMin(matrix);
     
-    for (let i = 0; i < matrix[0].length; i++){
-        let max = 0;
-        for (let j = 0; j < matrix.length; j++) {
+//     for (let i = 0; i < matrix[0].length; i++){
+//         let max = 0;
+//         for (let j = 0; j < matrix.length; j++) {
             
-            let col = matrix[j][i];
-            console.log(i)
-            if (max < col) max = col;    
+//             let col = matrix[j][i];
+//             console.log(i)
+//             if (max < col) max = col;    
           
-        } 
-        if(min.has(max)) {
-            return [max];
+//         } 
+//         if(min.has(max)) {
+//             return [max];
+//         }
+//     }
+//     return [];
+    const luckyNumbers = [];
+    
+    for (let i = 0; i < matrix.length; i++) {
+        const [minValue, columnIndex] = matrix[i].reduce((result, value, index) => {
+            return (!result[0] || value < result[0]) ? [value, index] : result;
+        }, []);
+        
+        for (let j = 0; j < matrix.length; j++) {
+            if (matrix[j][columnIndex] > minValue) {
+                break;
+            }
+            
+            if (j === matrix.length - 1) {
+                luckyNumbers.push(minValue);
+            }
         }
     }
-    return [];
+    
+    return luckyNumbers;
 };
