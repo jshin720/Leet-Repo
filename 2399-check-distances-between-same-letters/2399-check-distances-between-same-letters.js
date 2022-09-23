bf8f1a2f06904fafbb5f83f038cd4be69833a256
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-23 13:01:51 -0400

    Time: 87 ms (81.40%), Space: 46.2 MB (8.14%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.js
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.js
@@ -4,35 +4,33 @@
  * @return {boolean}
  */
 var checkDistances = function(s, distance) {
-//     const spaced = letterIdx(s);
-//     console.log(spaced)
+    const spaced = letterIdx(s);
+    console.log(spaced)
     
     
-//     for (let char in spaced) {
+    for (let char in spaced) {
         
-//         if (spaced[char] <= distance[char] ) {
-//             return false;
-//         } else if (spaced[char] >= 51) {
-//             return false;
-//         }
-//     }
-//     return true;
-// };
+        if (spaced[char] - 1 !== distance[char] ) {
+            return false;
+        } 
+    }
+    return true;
+};
 
-// const letterIdx = (s) => {
-//     const idx = {};
-//     for (let i = 0; i < s.length; i++) {
-//         let char = s[i].charCodeAt(0) - 97;
-//         if (!idx[char]) {
-//             idx[char] = i;
-//         } else {
-//             idx[char] = Math.abs(idx[char] - i);
-//         };
-//     }
-//     return idx;
+const letterIdx = (s) => {
+    const idx = {};
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i].charCodeAt(0) - 97;
+        if (!idx[char]) {
+            idx[char] = i;
+        } else {
+            idx[char] = Math.abs(idx[char] - i);
+        };
+    }
+    return idx;
     
-     for (let c of new Set(s))
-    if (s.lastIndexOf(c) - s.indexOf(c) - 1 !== distance[c.charCodeAt(0) - 97])
-      return false;
-  return true
+  //    for (let c of new Set(s))
+  //   if (s.lastIndexOf(c) - s.indexOf(c) - 1 !== distance[c.charCodeAt(0) - 97])
+  //     return false;
+  // return true
 }
