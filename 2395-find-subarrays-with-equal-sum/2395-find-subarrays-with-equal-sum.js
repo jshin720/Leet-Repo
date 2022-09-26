9d2f67091fb38b5d36465f1f1779fed05fccc060
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-26 13:30:54 -0400

    Time: 86 ms (75.15%), Space: 42.2 MB (59.34%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.js
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.js
@@ -3,28 +3,25 @@
  * @return {boolean}
  */
 var findSubarrays = function(nums) {
-//     const subSums = new Set() ;
+    const subSums = new Set() ;
     
-//     for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         
-//         for (let j = i + 1; j < nums.length; j++) {
+       
             
-//             let sum = nums[i] + nums[j];
+            let sum = nums[i] + nums[ i + 1];
             
-//             if (!subSums.has(sum)) {
-//                 subSums.add(sum);
-//             } else {
-//                 return true;
-//             }
-//         }
-//     }
-
-//     return false;
-     let s = new Set()
-    for(let i = 0; i<nums.length-1; i++){
-        let sum = nums[i] + nums[i+1]
-        if(s.has(sum)) return true
-        s.add(sum)
+            if (subSums.has(sum)) return true;
+                subSums.add(sum);
+        
     }
-    return false
+
+    return false;
+    //  let s = new Set()
+    // for(let i = 0; i<nums.length-1; i++){
+    //     let sum = nums[i] + nums[i+1]
+    //     if(s.has(sum)) return true
+    //     s.add(sum)
+    // }
+    // return false
 };
