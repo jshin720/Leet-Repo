672bea6e30bfd0cf3d42cc64b13656645a27f0fb
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-26 14:28:50 -0400

    Time: 197 ms (15.87%), Space: 51.5 MB (5.08%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {number}
+ */
+var countWords = function(words1, words2) {
+    let count = 0;
+    const dupes1 = noDupes(words1);
+    
+    const dupes2 = noDupes(words2);
+    
+    console.log(dupes1)
+    console.log(dupes2, "d2")
+    
+    for (let i = 0; i < words1.length; i++) {
+        if(dupes1[words1[i]] === 1 && dupes2[words1[i]] === 1) count++;
+    }
+    return count; 
+};
+
+const noDupes = (word) => {
+    const count = {};
+    
+    word.forEach((word) => {
+        !count[word] ? count[word] = 1 : count[word]++;
+    })
+    
+   
+    
+    return count; 
+}
