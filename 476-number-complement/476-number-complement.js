d9f18dea8ddf61d1fd26bf7472f80d2904eb0bff
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-27 12:51:47 -0400

    Time: 109 ms (14.08%), Space: 42.1 MB (51.91%) - LeetHub

---
+++ 476-number-complement/476-number-complement.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let index = 0;
+    let result = 0;
+    while (num) {
+        num /= 2;
+        result += !(num % 1) * 2 ** index++;
+        num = Math.floor(num);
+    };
+    return result;
+};
