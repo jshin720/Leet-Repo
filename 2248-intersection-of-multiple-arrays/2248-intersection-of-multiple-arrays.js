6dfdb38f5c55229fdf9cac8406d7ede2afd0d917
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-28 14:24:37 -0400

    Time: 178 ms (5.21%), Space: 49.5 MB (5.21%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
@@ -3,19 +3,33 @@
  * @return {number[]}
  */
 var intersection = function(nums) {
-    const firstNums = nums[0].sort((a, b) => a - b);
+//     const firstNums = nums[0].sort((a, b) => a - b);
     
-    const intersect = firstNums.filter((num) => {
-        // let j = 0;
-        // console.log("num", num)
-        for (let i = 1; i < nums.length; i++) {
-            // console.log("otherNum", nums[i])
-            if (!nums[i].includes(num)) {
-                return false;
-            }
+//     const intersect = firstNums.filter((num) => {
+//         // let j = 0;
+//         // console.log("num", num)
+//         for (let i = 1; i < nums.length; i++) {
+//             // console.log("otherNum", nums[i])
+//             if (!nums[i].includes(num)) {
+//                 return false;
+//             }
+//         }
+//         // j = 0;
+//         return true;
+//     })
+//    return intersect;
+    const intersect = {};
+    const flatten = nums.flat();
+    console.log(flatten)
+    
+    for (let i = 0; i < flatten.length; i++) {
+        if(!intersect[flatten[i]]) {
+            intersect[flatten[i]] = 1;
+        } else {
+            intersect[flatten[i]]++;
         }
-        // j = 0;
-        return true;
-    })
-   return intersect;
+    }
+    const common = Object.keys(intersect).filter((num) => intersect[num] === nums.length)
+    
+    return common
 };
