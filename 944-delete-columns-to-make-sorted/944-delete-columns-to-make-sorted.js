6a40683a15d6cd0437739b4f9c02fff6d6cccc50
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-28 13:54:35 -0400

    Time: 99 ms (77.01%), Space: 45.4 MB (97.70%) - LeetHub

--- 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.js
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.js
@@ -6,35 +6,45 @@
 // ["cba","daf","ghi"]
 // ["zyx","wvu","tsr"]
 var minDeletionSize = function(strs) {
-   const colsArr = [];
-    const wordsLen = strs[0].length;
+//    const colsArr = [];
+//     const wordsLen = strs[0].length;
     
-    for (let i = 0; i < wordsLen; i++) {
-        let words = "";
+//     for (let i = 0; i < wordsLen; i++) {
+//         let words = "";
         
-        // if (strs[i].length === 1) {
-        //     colsArr.push(strs[i])    
-        //     continue;
-        // }
+//         // if (strs[i].length === 1) {
+//         //     colsArr.push(strs[i])    
+//         //     continue;
+//         // }
         
-        for (let j = 0; j < strs.length; j++) {
-            words += strs[j][i];
-        }
-        colsArr.push(words);
-        words = "";
-    }
-    console.log(colsArr)
-    const minSize = colsArr.filter((word) => !sortedEquals(word));
+//         for (let j = 0; j < strs.length; j++) {
+//             words += strs[j][i];
+//         }
+//         colsArr.push(words);
+//         words = "";
+//     }
+//     console.log(colsArr)
+//     const minSize = colsArr.filter((word) => !sortedEquals(word));
     
-    return minSize.length;
-};
+//     return minSize.length;
+// };
 
 
-const sortedEquals = (str) => {
-    // const sorted = str.split("").sort()
+// const sortedEquals = (str) => {
+//     // const sorted = str.split("").sort()
    
-    for (let i = 0; i < str.length - 1; i++) {
-        if (str[i] > str[i + 1]) return false;
+//     for (let i = 0; i < str.length - 1; i++) {
+//         if (str[i] > str[i + 1]) return false;
+//     }
+//     return true;     
+    let num = 0;
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] < strs[j-1][i]) {
+                num ++;
+                break;
+            }
+        }
     }
-    return true;     
+    return num;
 } 
