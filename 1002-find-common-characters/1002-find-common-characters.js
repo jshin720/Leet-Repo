81873a60ff9b7c2d5bb8906ef67930aa8e652b27
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-28 13:00:11 -0400

    Time: 104 ms (82.08%), Space: 45.8 MB (67.69%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.js
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -3,7 +3,42 @@
  * @return {string[]}
  */
 var commonChars = function(words) {
-    const common = words[0].split("").filter((char) => {
+//      let array =[];
+    
+//     for(let i =0;i < words[0].length ; i++){
+        
+//         let  check = false;
+//         for(let j =0; j < words.length-1; j++){
+            
+//             if(words[j+1].includes(words[0][i])){
+//                words[j+1] = words[j+1].replace(words[0][i],"");
+//                 check= true;
+                
+//             } else{
+//                 check = false;
+//                 break;
+//             }
+//         }
+        
+//         if(check){
+//             array.push(words[0][i]);
+//         }
+        
+//     }
+    
+//     return array;
+//     const common = new Map();
+    
+//     for (let i = words.length - 1; i >= 1; i--) {
+//         const main = words[0];
+        
+//         for (let j = 0; j < words[i].length; j++) {
+//             if (main.includes(words[i][j])) {
+                
+//             }
+//         }
+//     }
+  const common = words[0].split("").filter((char) => {
         // console.log("char", char)
         for (let i = 1; i < words.length; i++) {
             // console.log("words", words[i], i)
@@ -19,3 +54,18 @@ var commonChars = function(words) {
     })
      return common
 };
+    
+
+
+// const lettersCount = (word) => {
+//     const count = {};
+    
+//     for (let char of word) {
+//         if (!count[char]) {
+//             count[char] = 1;
+//         } else {
+//             count[char]++;
+//         };
+//     };
+//     return count; 
+// };
