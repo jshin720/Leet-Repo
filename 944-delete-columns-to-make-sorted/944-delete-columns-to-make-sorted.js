819d82e684f132b17301d0c8402cf6a5a623f6fc
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-28 13:49:05 -0400

    Time: 256 ms (20.69%), Space: 61.6 MB (6.32%) - LeetHub

---
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+ 
+// ["cba","daf","ghi"]
+// ["zyx","wvu","tsr"]
+var minDeletionSize = function(strs) {
+   const colsArr = [];
+    const wordsLen = strs[0].length;
+    
+    for (let i = 0; i < wordsLen; i++) {
+        let words = "";
+        
+        // if (strs[i].length === 1) {
+        //     colsArr.push(strs[i])    
+        //     continue;
+        // }
+        
+        for (let j = 0; j < strs.length; j++) {
+            words += strs[j][i];
+        }
+        colsArr.push(words);
+        words = "";
+    }
+    console.log(colsArr)
+    const minSize = colsArr.filter((word) => !sortedEquals(word));
+    
+    return minSize.length;
+};
+
+
+const sortedEquals = (str) => {
+    const sorted = str.split("").sort()
+   
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== sorted[i]) return false;
+    }
+    return true;     
+} 
