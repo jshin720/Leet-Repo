2580409a21cd22b3971e720c266ceaff0c8ad1da
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-28 12:59:33 -0400

    Time: 403 ms (5.19%), Space: 50 MB (25.00%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.js
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -3,69 +3,19 @@
  * @return {string[]}
  */
 var commonChars = function(words) {
-//      let array =[];
-    
-//     for(let i =0;i < words[0].length ; i++){
-        
-//         let  check = false;
-//         for(let j =0; j < words.length-1; j++){
-            
-//             if(words[j+1].includes(words[0][i])){
-//                words[j+1] = words[j+1].replace(words[0][i],"");
-//                 check= true;
-                
-//             } else{
-//                 check = false;
-//                 break;
-//             }
-//         }
-        
-//         if(check){
-//             array.push(words[0][i]);
-//         }
-        
-//     }
-    
-//     return array;
-//     const common = new Map();
-    
-//     for (let i = words.length - 1; i >= 1; i--) {
-//         const main = words[0];
-        
-//         for (let j = 0; j < words[i].length; j++) {
-//             if (main.includes(words[i][j])) {
-                
-//             }
-//         }
-//     }
- let common = words[0].split('').filter(char => {
-      
-        for (var i = 1; i < words.length; i++) {
-         
-            if (!words[i].includes(char)) return false;
-            else {
-                words[i] = words[i].replace(char, '')
+    const common = words[0].split("").filter((char) => {
+        console.log("char", char)
+        for (let i = 1; i < words.length; i++) {
+            console.log("words", words[i], i)
+            if (!words[i].includes(char)){
+                return false;
+            } else {
+                words[i] = words[i].replace(char, "");
             }
+            // console.log("after", i)
         }
-    
         return true;
-     
+       
     })
- // console.log("main", common)
-	return common
+     return common
 };
-    
-
-
-// const lettersCount = (word) => {
-//     const count = {};
-    
-//     for (let char of word) {
-//         if (!count[char]) {
-//             count[char] = 1;
-//         } else {
-//             count[char]++;
-//         };
-//     };
-//     return count; 
-// };
