7a6b86ad0e622050096a7467543e26fc0b1dd207
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-28 14:16:30 -0400

    Time: 677 ms (5.21%), Space: 52.3 MB (5.21%) - LeetHub

---
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+var intersection = function(nums) {
+    const firstNums = nums[0];
+    
+    const intersect = firstNums.filter((num) => {
+        // let j = 0;
+        console.log("num", num)
+        for (let i = 1; i < nums.length; i++) {
+            console.log("otherNum", nums[i])
+            if (!nums[i].includes(num)) {
+                return false;
+            }
+        }
+        // j = 0;
+        return true;
+    })
+   return intersect.sort((a, b) => a - b);
+};
