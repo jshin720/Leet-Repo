1799a7729fbb7ad7e78132268f516238806e245f
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-29 12:58:38 -0400

    Time: 98 ms (41.15%), Space: 42.1 MB (52.21%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.js
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -3,8 +3,15 @@
  * @return {boolean}
  */
 var threeConsecutiveOdds = function(arr) {
-    for (let i = 2; i < arr.length; i++) {
-        if (arr[i] & 1 && arr[i - 1] & 1 && arr[i - 2] & 1) {
+//     for (let i = 2; i < arr.length; i++) {
+//         if (arr[i] & 1 && arr[i - 1] & 1 && arr[i - 2] & 1) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+     for (i = 0; i < arr.length - 2; i++) {
+        if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i + 2] % 2 !== 0) {
             return true;
         }
     }
