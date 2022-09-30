489ea8b45a58761fd0ada2413e6dc1f527a1bdb4
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-30 13:34:12 -0400

    Time: 262 ms (5.48%), Space: 49.3 MB (5.19%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMiddleIndex = function(nums) {
+    const sum = nums.reduce((a , b) => a + b, 0);
+    let leftSum = 0;
+    
+    
+    for (let i = 0; i < nums.length; i++) {
+        let rightSum = sum - leftSum;
+        leftSum += nums[i];
+        if (leftSum === rightSum) return i;
+        
+        
+        
+        console.log("rs", rightSum);
+        console.log("ls", leftSum)
+    }
+    
+    return -1;
+    
+    
+    
+};
