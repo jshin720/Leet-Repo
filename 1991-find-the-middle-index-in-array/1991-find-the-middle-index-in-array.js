6e27517edba6c3fd1073361dedc53de1df94bf52
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-30 13:59:26 -0400

    Time: 129 ms (12.39%), Space: 42.5 MB (45.82%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
@@ -3,23 +3,37 @@
  * @return {number}
  */
 var findMiddleIndex = function(nums) {
-    const sum = nums.reduce((a , b) => a + b, 0);
+//     const sum = nums.reduce((a , b) => a + b, 0);
+//     let leftSum = 0;
+    
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         let rightSum = sum - leftSum;
+//         leftSum += nums[i];
+//         if (leftSum === rightSum) return i;
+        
+        
+        
+//         // console.log("rs", rightSum);
+//         // console.log("ls", leftSum)
+//     }
+    
+//     return -1;
+    let sum = nums.reduce((sum,a) => sum+=a,0);
     let leftSum = 0;
+    let midIndex = -1;
     
-    
-    for (let i = 0; i < nums.length; i++) {
-        let rightSum = sum - leftSum;
-        leftSum += nums[i];
-        if (leftSum === rightSum) return i;
+    for(let i=0; i < nums.length; i++) {
         
+        let rightSum = sum-leftSum-nums[i];
+        // console.log(rightSum)
+        if(rightSum === leftSum) {
+            return i;
+        } else {
+            leftSum+= nums[i];
+        }
         
-        
-        // console.log("rs", rightSum);
-        // console.log("ls", leftSum)
     }
     
-    return -1;
-    
-    
-    
+    return midIndex;
 };
