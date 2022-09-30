fec4d8f24213d2e20a12175f97ba1ee1b196ca00
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-09-30 14:22:37 -0400

    Time: 108 ms (25.70%), Space: 41.8 MB (93.03%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -3,13 +3,25 @@
  * @return {number}
  */
 var maxDepth = function(s) {
+//     let depth = 0;
+//     let maxDepth = 0;
+    
+//     for (let char of s ) {
+//         if (char === "(") depth++;
+//         if (char === ")") depth--;
+//         depth > maxDepth ? maxDepth = depth : maxDepth;
+//     }
+//     return maxDepth; 
+    
     let depth = 0;
     let maxDepth = 0;
     
-    for (let char of s ) {
-        if (char === "(") depth++;
-        if (char === ")") depth--;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") depth++;
+        if (s[i] === ")") depth--;
+        
         depth > maxDepth ? maxDepth = depth : maxDepth;
-    }
-    return maxDepth; 
+    } 
+    
+    return maxDepth
 };
