d318867783cc25d746717ac3608df55f288c99e9
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-03 17:15:30 -0400

    Time: 129 ms (39.39%), Space: 47.3 MB (12.12%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.js
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.js
@@ -4,15 +4,29 @@
  */
 var countGoodRectangles = function(rectangles) {
  
+//     const largestSq = rectangles.map((rectangle) => Math.min(rectangle[0], rectangle[1]));                       
+//     const maxSize = Math.max(...largestSq);
     
-    const largestSq = rectangles.map((rectangle) => Math.min(rectangle[0], rectangle[1]));
-                               
-    const maxSize = Math.max(...largestSq);
+//     let count = 0;
     
-    let count = 0;
+//     for (let sq of largestSq) {
+//         if(sq === maxSize) count++;
+//     }
     
-    for (let sq of largestSq) {
-        if(sq === maxSize) count++;
+//     return count; 
+    
+    let maxSize = 0
+    
+    // size => count of size
+    const map = new Map()
+    for (const rectangle of rectangles.values()) {
+        const [height, width] = rectangle
+        const size = Math.min(height, width)
+        map.set(size, 1 + (map.get(size) || 0))
+        maxSize = Math.max(maxSize, size)
     }
-    return count; 
+    
+    
+    let result = map.get(maxSize)
+    return result
 };
