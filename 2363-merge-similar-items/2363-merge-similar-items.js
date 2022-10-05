a676abac2a3fdb17d9af8167f6753b593d4a7087
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-05 14:02:44 -0400

    Time: 172 ms (41.90%), Space: 48.9 MB (77.56%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.js
+++ 2363-merge-similar-items/2363-merge-similar-items.js
@@ -4,13 +4,27 @@
  * @return {number[][]}
  */
 var mergeSimilarItems = function(items1, items2) {
-    let map = {};
-    for (let [value, weight] of items1.concat(items2)) {
-        if (!map[value]) {
-            map[value] = weight;
+    const similar = {};
+    
+    for (let i = 0; i < items1.length; i++) {
+        const [v, w] = items1[i];
+        
+        if (!similar[v]) {
+            similar[v] = w;
         } else {
-            map[value] += weight;
+            similar[v] += w;
         }
     }
-    return Object.keys(map).sort((a, b) => a - b).map(key => [key, map[key]])
+    
+    for (let i = 0; i < items2.length; i++) {
+        const [v, w] = items2[i];
+        
+        if (!similar[v]) {
+            similar[v] = w;
+        } else {
+            similar[v] += w;
+        }
+    }
+   
+    return Object.entries(similar);
 };
