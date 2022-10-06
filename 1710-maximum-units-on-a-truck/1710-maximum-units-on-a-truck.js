75c60818b7cdc189f884cda9c443bc443499dc1e
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-06 14:43:38 -0400

    Time: 477 ms (5.05%), Space: 51.1 MB (5.16%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
@@ -4,22 +4,44 @@
  * @return {number}
  */
 var maximumUnits = function(boxTypes, truckSize) {
-    boxTypes.sort((a, b) => b[1] - a[1]);
-    let total = 0;
+//     boxTypes.sort((a, b) => b[1] - a[1]);
+//     let total = 0;
     
-    for (let i = 0; i < boxTypes.length; i++) {
-        let num = boxTypes[i][0];
-        let units = boxTypes[i][1];
+//     for (let i = 0; i < boxTypes.length; i++) {
+//         let num = boxTypes[i][0];
+//         let units = boxTypes[i][1];
         
-        if (num <= truckSize) {
-            total += num * units;
-            truckSize -= num;
+//         if (num <= truckSize) {
+//             total += num * units;
+//             truckSize -= num;
+//         } else {
+//             total += truckSize * units;
+//             truckSize = 0;
+//         }
+        
+//     }
+    
+//     return total;
+    
+    
+    boxTypes.sort ((a, b) => b[1] - a[1]);
+    let total = 0;
+    // console.log(boxTypes)
+    
+    for (let boxType of boxTypes) {
+        // console.log(boxType)
+        
+        const numBoxes = boxType[0];
+        const unit = boxType[1];
+        console.log("truckSize", truckSize)
+        if (numBoxes <= truckSize) {
+            total += numBoxes * unit;
+            truckSize -= numBoxes;
         } else {
-            total += truckSize * units;
+            total += truckSize * unit;
             truckSize = 0;
         }
-        
-    }
-    
+        console.log(total)
+    } 
     return total;
 };
