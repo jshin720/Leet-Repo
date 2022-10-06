14459d28b1c3f6a90d99f6c7cc990564c200b948
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-06 14:14:21 -0400

    Time: 124 ms (15.43%), Space: 42.5 MB (62.09%) - LeetHub

--- 682-baseball-game/682-baseball-game.js
+++ 682-baseball-game/682-baseball-game.js
@@ -3,18 +3,37 @@
  * @return {number}
  */
 var calPoints = function(ops) {
+//     const record = [];
+    
+//     for (const i in ops) {
+//       if (ops[i] === "C" ) {
+//           record.pop();
+//       }  else if (ops[i] === "D") {
+//          record.push(record[record.length - 1] * 2);
+//       } else if (ops[i] === "+") {
+//           record.push(record[record.length - 1] + record[record.length -2]);
+//       } else {
+//           record.push(+ops[i]);
+//       }
+//     }
+    
+//     return record.reduce((a, b) => a + b, 0);
+    
     const record = [];
     
-    for (const i in ops) {
-      if (ops[i] === "C" ) {
-          record.pop();
-      }  else if (ops[i] === "D") {
-         record.push(record[record.length - 1] * 2);
-      } else if (ops[i] === "+") {
-          record.push(record[record.length - 1] + record[record.length -2]);
-      } else {
-          record.push(+ops[i]);
-      }
+    for (let op of ops) {
+        const lastOp = Number(record[record.length - 1]);
+        
+        if (op === "C") {
+            record.pop();
+        } else if ( op === "D") {
+            record.push(lastOp * 2);
+        } else if (op === "+") {
+            const sum = lastOp + Number(record[record.length - 2]);
+            record.push(sum);
+        } else {
+            record.push(Number(op));
+        }
     }
     
     return record.reduce((a, b) => a + b, 0);
