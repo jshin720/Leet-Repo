32aa2ffc6bca75f697a74014ea491adfddc3ed01
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-10 18:08:25 -0400

    Time: 100 ms (65.91%), Space: 45 MB (19.69%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
@@ -2,10 +2,15 @@ function runningSum(nums: number[]): number[] {
     const ans: number[] = [];
     let sum: number = 0;
     
-    for (let num of nums) {
+    // for (let num of nums) {
+    //     sum += num;
+    //     ans.push(sum);
+    // };
+    
+    nums.forEach((num) => {
         sum += num;
-        ans.push(sum);
-    };
+        ans.push(sum)
+    })
     
     return ans;
 };
