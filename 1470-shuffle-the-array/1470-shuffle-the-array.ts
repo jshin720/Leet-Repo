88b72309e8660930cbc9922c54cf956bfaad3ca8
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-11 01:01:13 -0400

    Time: 65 ms (99.36%), Space: 44.9 MB (64.10%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.ts
+++ 1470-shuffle-the-array/1470-shuffle-the-array.ts
@@ -3,7 +3,7 @@ function shuffle(nums: number[], n: number): number[] {
     
     const xArr: number[] = nums.slice(0, n)
     const yArr: number[] = nums.slice(n);
-    console.log(yArr);
+    // console.log(yArr);
     
     for (let i = 0; i < xArr.length; i++) {
         shuffled.push(xArr[i]);
