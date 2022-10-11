61ed98d81ec475e79acda86478a881dcac5d74bd
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-11 00:49:06 -0400

    Time: 77 ms (88.75%), Space: 45.2 MB (10.63%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.ts
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.ts
@@ -2,11 +2,13 @@ function finalValueAfterOperations(operations: string[]): number {
     let x: number = 0;
     
     for (let op of operations) {
-        if (op.includes("+")) {
-            x++;
-        } else {
-            x--;
-        }
+        // if (op.includes("+")) {
+        //     x++;
+        // } else {
+        //     x--;
+        // }
+        
+        op.startsWith("+") || op.endsWith("+") ? x++ : x--;
     }
     return x;
 };
