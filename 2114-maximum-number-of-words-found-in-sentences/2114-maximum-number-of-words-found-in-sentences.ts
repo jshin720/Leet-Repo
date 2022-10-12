b1952e32148f731cc11f7faf088018b2270e39b2
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-12 00:55:58 -0400

    Time: 132 ms (18.24%), Space: 45.1 MB (48.65%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.ts
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.ts
@@ -1,6 +1,5 @@
 function mostWordsFound(sentences: string[]): number {
-    let count: number = 0, max: number = 0;
-    
+
     const wordCount = sentences.map((sent) => {
         return sent.split(" ").length;
     })
