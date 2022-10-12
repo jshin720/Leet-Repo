513b175f47e7de191983a868ccaf02323597dc02
jshin720 <80377105+jshin720@users.noreply.github.com>
2022-10-12 00:43:29 -0400

    Time: 106 ms (46.76%), Space: 44.7 MB (36.81%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.ts
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.ts
@@ -1,11 +1,14 @@
 function maximumWealth(accounts: number[][]): number {
-    const totalWealth: number[] = [];
+    // const totalWealth: number[] = [];
+    let max: number = 0;
     
     for (let i = 0; i < accounts.length; i++) {
         let sum = accounts[i].reduce((a, b) => a + b);
         
-        totalWealth.push(sum);
+        // totalWealth.push(sum);
+        max = Math.max(max, sum);
     }
     
-    return Math.max(...totalWealth);
+//     return Math.max(...totalWealth);
+    return max;
 };
