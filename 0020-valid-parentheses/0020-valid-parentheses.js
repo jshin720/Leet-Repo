ca08ab1f7b481fd18dfd9a3bf282a7aef1b7e153
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-02-07 17:30:57 -0500

    Time: 73 ms (56.08%), Space: 42.3 MB (72.03%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,60 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+//      if (s.length === 0 || s.length % 2 !== 0){
+//         return false;
+//     }
+//     const sArr = s.split("")
+//     const validArr = [sArr.shift()];
+//     // const counter = {};
+
+//     for (let i = 0; i < sArr.length; i++) {
+//         // if (!counter[s[i]]) {
+//         //     counter[s[i]] = 1;
+//         // } else {
+//         //     counter[s[i]]++;
+//         // }
+//         if (sArr[i] === ")" && validArr[validArr.length - 1] === "(") {
+//             validArr.pop();
+//         } else if (sArr[i] === "}" && validArr[validArr.length - 1] === "{") {
+//             validArr.pop();
+//         } else if (sArr[i] === "]" && validArr[validArr.length - 1] === "["){
+//             validArr.pop();
+//         } else {
+//             validArr.push(sArr[i])
+//         }
+       
+//     }
+
+    // console.log(validArr)
+
+    // for (let str in counter) {
+    //     if (counter["("] !== counter[")"]) return false;
+    //     if (counter["{"] !== counter["}"]) return false;
+    //     if (counter["["] !== counter["]"]) return false;
+    // }
+
+    // return validArr.length === 0
+    if (s.length === 0 || s.length % 2 !== 0) return false;
+    let sArr = s.split("")
+    const valid = [s[0]];
+    
+    let left = 0;
+    
+    for (let right = 1; right < s.length; right++) {
+        if (s[right] === ")" && valid[valid.length - 1] === "(") {
+            valid.pop();
+        } else if (s[right] === "]" && valid[valid.length - 1] === "[") {
+            valid.pop();
+        } else if (s[right] === "}" && valid[valid.length - 1] === "{") {
+            valid.pop();
+        } else {
+            valid.push(s[right]);
+        }
+          
+    }
+    
+    return valid.length === 0;
+};
