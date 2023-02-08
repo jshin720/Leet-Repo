5ce14a5f538950918b220f346b6127dcfeb7324d
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-02-08 14:50:42 -0500

    Time: 33 ms (69.58%), Space: 13.9 MB (19.30%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,32 +1,54 @@
 class Solution:
     def isValid(self, s: str) -> bool:
        
-        if len(s) == 0 or len(s) % 2 != 0:
-            return False
+#         if len(s) == 0 or len(s) % 2 != 0:
+#             return False
       
-        s_arr = list(s)
-        valid = [s_arr.pop(0)]
+#         s_arr = list(s)
+#         valid = [s_arr.pop(0)]
         
-        for i in s_arr:
+#         for i in s_arr:
            
-            if i == ")":
-                if valid and valid[-1] == "(":
-                    valid.pop()
-                else:
-                    return False
-            elif i == "}":
-                if valid and valid[-1] == "{":
-                    valid.pop()
-                else:
-                    return False
-            elif i == "]":
-                if valid and valid[-1] == "[":
-                    valid.pop()
+#             if i == ")":
+#                 if valid and valid[-1] == "(":
+#                     valid.pop()
+#                 else:
+#                     return False
+#             elif i == "}":
+#                 if valid and valid[-1] == "{":
+#                     valid.pop()
+#                 else:
+#                     return False
+#             elif i == "]":
+#                 if valid and valid[-1] == "[":
+#                     valid.pop()
+#                 else:
+#                     return False
+#             else:
+#                 valid.append(i)
+        
+#         # print(valid)
+        
+#         return len(valid) == 0
+
+
+        valid_char = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+          }
+
+        stack = list()
+        
+       
+        for char in s:
+            if char in valid_char:
+                if len(stack) > 0 and stack[len(stack) - 1] == valid_char[char]:
+                    stack.pop()
                 else:
                     return False
             else:
-                valid.append(i)
+                stack.append(char)
+                
         
-        # print(valid)
-        
-        return len(valid) == 0
+        return len(stack) == 0
