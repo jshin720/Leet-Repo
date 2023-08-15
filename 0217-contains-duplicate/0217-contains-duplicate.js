a16976c5a9ae55902385705657a6aa24d8ad4691
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-08-15 16:05:41 -0400

    Time: 83 ms (78.31%), Space: 52.7 MB (72.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,19 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-//     const window = new Set();
-
-//     for (let right = 0; right < nums.length; right++) {
-//         if (window.has(nums[right])) return true;
-//         window.add(nums[right]);
-//     }
-//     return false;
-    
-    const anyDupes = new Set();
+    const window = new Set();
     
     for (let i = 0; i < nums.length; i++) {
-        if (anyDupes.has(nums[i])) return true;
-        anyDupes.add(nums[i]);
+        if (window.has(nums[i])) return true;
+        window.add(nums[i]);
     }
     return false;
 };
