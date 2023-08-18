4125f08d888ba65c65423c2753e20465f00e5cda
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-08-18 13:50:00 -0400

    Time: 84 ms (27.82%), Space: 48.8 MB (14.19%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const count = new Map();
+    // const sorted = [...new Set(nums)];
+    
+    for (let num of nums) {
+        if (!count.has(num)) {
+            count.set(num, 1);
+        } else {
+            count.set(num, count.get(num) + 1);
+        }
+    }
+    
+    const sorted = [];
+    for (let [key, value] of count) {
+        sorted.push([key, value]);
+    }
+      console.log(count)
+      console.log(sorted)
+    
+    const mapped = sorted.sort((a, b) => b[1] - a[1])
+    console.log(mapped)
+    let res = [];
+    
+
+        for (let num of mapped) {
+            res.push(num[0]);
+        }
+   
+    return res.slice(0, k)
+};
