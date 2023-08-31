049b71a43f42c0cbd5d8acc6ca5d023ee48e3ad4
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-08-31 15:37:04 -0400

    Time: 99 ms (50.39%), Space: 56 MB (18.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,23 +3,42 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const left = new Array(nums.length).fill(0);
-    left[0] = 1;
-    const right = new Array(nums.length).fill(0);
-    right[right.length - 1] = 1;
+//     const left = new Array(nums.length).fill(0);
+//     left[0] = 1;
+//     const right = new Array(nums.length).fill(0);
+//     right[right.length - 1] = 1;
     
     
-    for (let i = 1;i < nums.length; i++) {
-        left[i] = nums[i - 1] * left[i - 1];
-    }
+//     for (let i = 1;i < nums.length; i++) {
+//         left[i] = nums[i - 1] * left[i - 1];
+//     }
     
-    for (let i = right.length - 2; i >= 0; i--) {
-        right[i] = nums[i + 1] * right[i + 1];
-    }
+//     for (let i = right.length - 2; i >= 0; i--) {
+//         right[i] = nums[i + 1] * right[i + 1];
+//     }
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         nums[i] = left[i] * right[i];
+//     }
+    
+//     return nums;
+    
+    let res = [];
+    let start = 1;
     
     for (let i = 0; i < nums.length; i++) {
-        nums[i] = left[i] * right[i];
+        res.push(start);
+        start = start *nums[i];
     }
     
-    return nums;
+   
+    let revStart = 1;
+    
+    for (let i = nums.length - 1; i >=0 ; i--) {
+        res[i] = revStart * res[i];
+        revStart = revStart * nums[i];
+    }
+    
+    
+    return res;
 };
