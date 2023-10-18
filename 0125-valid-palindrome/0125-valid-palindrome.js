7bdb044c176eb0fc92d78c5788408fb371b55fc3
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-10-18 14:47:01 -0400

    Time: 180 ms (5.05%), Space: 51.2 MB (9.05%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const alphaNum = (char) => (
+     char.toLowerCase() >= "a" && char.toLowerCase() <= "z" || char >= "0" && char <= "9"
+)
+   
+
+
+var isPalindrome = function(s) {
+    
+    let begin = 0, end = s.length - 1;
+    
+    while(begin < end) {
+       
+        if (!alphaNum(s[begin])) {
+            begin++
+            continue
+        }
+        if(!alphaNum(s[end])) {
+            end--
+            continue;
+        }
+         console.log(s[begin], s[end])
+        if (s[begin].toLowerCase() !== s[end].toLowerCase()) return false;
+        begin++;
+        end--;
+    }   
+    return true; 
+};
+
