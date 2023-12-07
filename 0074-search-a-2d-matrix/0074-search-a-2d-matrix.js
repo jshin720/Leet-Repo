b1f08fb7f7e12f350e99bae96e6d15f513d0788a
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-12-07 12:21:34 -0500

    Time: 54 ms (44.67%), Space: 42.5 MB (21.30%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -5,19 +5,37 @@
  */
 var searchMatrix = function(matrix, target) {
     
-    for (let i = 0; i < matrix.length; i++) {
-        let matrices = matrix[i]
+//     for (let i = 0; i < matrix.length; i++) {
+//         let matrices = matrix[i]
         
-        if (matrices[matrices.length - 1] < target) continue;
+//         if (matrices[matrices.length - 1] < target) continue;
         
-        let mid = Math.floor(matrices.length / 2);
+//         let mid = Math.floor(matrices.length / 2);
+//         let left = 0;
+//         let right = matrices.length - 1;
+        
+//         while (left <= right) {
+            
+//             if (target === matrices[mid]) return true;
+//             if (target > matrices[mid]) {
+//                 mid++;
+//                 left = mid;
+//             } else {
+//                 mid--;
+//                 right = mid;
+//             }
+//         }
+//     }
+    
+    const newArr = matrix.flat();
+        let mid = Math.floor(newArr.length / 2);
         let left = 0;
-        let right = matrices.length - 1;
+        let right = newArr.length - 1;
         
         while (left <= right) {
             
-            if (target === matrices[mid]) return true;
-            if (target > matrices[mid]) {
+            if (target === newArr[mid]) return true;
+            if (target > newArr[mid]) {
                 mid++;
                 left = mid;
             } else {
@@ -25,7 +43,6 @@ var searchMatrix = function(matrix, target) {
                 right = mid;
             }
         }
-    }
     
     return false;
 };
