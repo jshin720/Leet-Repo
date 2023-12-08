abdca0052cd79476cc87dcaec96223aaef52c2e4
jshin720 <80377105+jshin720@users.noreply.github.com>
2023-12-08 11:10:27 -0500

    Time: 62 ms (79.96%), Space: 45.3 MB (44.44%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+//     let bph = 0;//bananas per hour
+//     let mid = Math.floor(piles.length / 2);
+    
+//     let min = 1;
+//     let max = Math.max(...piles);
+    
+//     while (min < max) {
+        
+//     }
+    
+//     const hrSpent = (mid, piles, hourSpent = 0) => {
+//         for (let pile of piles) {
+//             hourSpent += Math.ceil(pile /mid)
+//         }
+//         return hourSpent;
+//     }
+    
+
+// };
+    
+        let [left, right] = [1, Math.max(...piles)];
+
+    while (left < right) {
+        const mid = (left + right) >> 1;
+        const hourSpent = getHourSpent(mid, piles);
+
+        const isTargetGreater = h < hourSpent;
+        if (isTargetGreater) left = mid + 1;
+
+        const isTargetLess = hourSpent <= h;
+        if (isTargetLess) right = mid;
+    }
+
+    return right;
+};
+
+const getHourSpent = (mid, piles, hourSpent = 0) => {
+    for (const pile of piles) {
+        hourSpent += Math.ceil(pile / mid);
+    }
+
+    return hourSpent;
+};
