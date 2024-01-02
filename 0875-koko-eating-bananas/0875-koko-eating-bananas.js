881d01552e9fd3994cc16a87619b7241dab03682
jshin720 <80377105+jshin720@users.noreply.github.com>
2024-01-02 13:45:22 -0500

    Time: 109 ms (12.15%), Space: 45 MB (77.28%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.js
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -4,43 +4,34 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-//     let bph = 0;//bananas per hour
-//     let mid = Math.floor(piles.length / 2);
-    
+    let bph
     let left = 1;
     let right = Math.max(...piles);
     
-//     while (min < max) {
-        
-//     }
-    
-//     const hrSpent = (mid, piles, hourSpent = 0) => {
-//         for (let pile of piles) {
-//             hourSpent += Math.ceil(pile /mid)
-//         }
-//         return hourSpent;
-//     }
-    
-
-// };
-    
-
     while (left < right) {
         const mid = Math.floor((left + right) / 2) 
         const hourSpent = getHourSpent(mid, piles);
-
-        if (h < hourSpent) left = mid + 1;
-
-        if (hourSpent <= h) right = mid;
+        // console.log("mid", mid)
+// console.log(hourSpent)
+        
+        if (hourSpent > h) {
+            left = mid + 1;
+        } else if (hourSpent <= h) {
+            right = mid;  
+        } 
+        
+        // console.log("left", left)
+    // console.log("right", right)
     }
-
+    
+ 
     return right;
 };
 
-const getHourSpent = (mid, piles, hourSpent = 0) => {
-    for (const pile of piles) {
-        hourSpent += Math.ceil(pile / mid);
-    }
+    const getHourSpent = (mid, piles, hourSpent = 0) => {
+        for (const pile of piles) {
+            hourSpent += Math.ceil(pile / mid);
+     }
 
     return hourSpent;
 };
