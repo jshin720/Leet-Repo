19afeffcaf8251937c4f6c2eef1fb030f8a709d7
jshin720 <80377105+jshin720@users.noreply.github.com>
2024-01-04 10:08:08 -0500

    Time: 68 ms (8.82%), Space: 43.8 MB (6.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -7,59 +7,33 @@ var search = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
     
-//     while (left <= right) {
-//         let mid = Math.floor((left + right) / 2);
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
         
-//         if (nums[mid] === target) return mid;
+        const midNum = nums[mid];
+        const leftNum = nums[left];
+        const rightNum = nums[right];
+        console.log(mid)
+        if (target === midNum) return mid;
         
-//         if (nums[left] <= nums[mid]) {
-//             if (nums[left] <= target && target < nums[mid]) {
-//                 right = mid--;
-//             } else {
-//                 left = mid++;
-//             }
-//         } else {
-//             if (nums[mid] < target && target <= nums[right]) {
-//                 left = mid++;
-//             }  else {
-//                 right = mid--;
-//             }
-//         }
-//     }
-    
-     while (left <= right) {
-        const mid = (left + right) >> 1;
-        const guess = nums[mid];
-        let leftNum = nums[left];
-        let rightNum = nums[right];
-
-        const isTarget = guess === target;
-        if (isTarget) return mid;
-
-        const isAscending = leftNum <= guess;
-        if (isAscending) {
-            const isInRange = leftNum <= target;
-            const isLess = target < guess;
-
-            const isTargetGreater = !(isInRange && isLess);
-            if (isTargetGreater) left = mid + 1;
-
-            const isTargetLess = isInRange && isLess;
-            if (isTargetLess) right = mid - 1;
-        }
-
-        const isDescending = guess < leftNum;
-        if (isDescending) {
-            const isGreater = guess < target;
-            const isInRange = target <= rightNum;
-
-            const isTargetGreater = isGreater && isInRange;
-            if (isTargetGreater) left = mid + 1;
-
-            const isTargetLess = !(isGreater && isInRange);
-            if (isTargetLess) right = mid - 1;
-        }
+        if (leftNum <= midNum) {
+            console.log("left")
+            if (target > midNum || target < leftNum) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            console.log("right")
+            if (target < midNum || target > rightNum) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } 
+        
+        
     }
-
+     
     return -1;
 };
