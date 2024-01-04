bb9e0818aee8a758405fcc8625d9c59323b19c8d
jshin720 <80377105+jshin720@users.noreply.github.com>
2024-01-04 09:00:24 -0500

    Time: 41 ms (97.23%), Space: 42.1 MB (43.73%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let [left, right] = [0, nums.length - 1];
-
+   let left = 0;
+   let right = nums.length - 1;
+    
     while (left < right) {
-        const mid = (left + right) >> 1;
-        const guess = nums[mid];
-        const [leftNum, rightNum] = [nums[left], nums[right]];
+        const mid = Math.floor((right + left) / 2);
+        const midNum = nums[mid];
+        const leftNum = nums[left];
+        const rightNum = nums[right];
 
-        const isTarget = leftNum < rightNum;
-        if (isTarget) return leftNum;
+        if (leftNum < rightNum) return leftNum;
+        
+        if (leftNum <= midNum) left = mid + 1;
 
-        const isTargetGreater = leftNum <= guess;
-        if (isTargetGreater) left = mid + 1;
-
-        const isTargetLess = guess < leftNum;
-        if (isTargetLess) right = mid;
+        if (midNum < leftNum) right = mid; 
+       
     }
-
     return nums[left];
 };
+
