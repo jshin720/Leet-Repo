a3a05c36b9e2e6fbcb19f5443d8f7702ff987473
jshin720 <80377105+jshin720@users.noreply.github.com>
2024-01-10 15:16:21 -0500

    Time: 76 ms (71.83%), Space: 49.8 MB (81.02%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    let slow = head;
+    let fast = head.next;
+    
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        
+    }
+    // console.log(slow)
+    let secondHalf = slow.next;
+    slow.next = null;
+    let prev = null;
+    
+    while (secondHalf) {
+        let temp = secondHalf.next;
+        secondHalf.next = prev;
+        prev = secondHalf;
+        secondHalf = temp;
+            
+    } 
+
+    // console.log(prev, "2ndhalf")
+    
+    let firstHalf = head;
+    secondHalf = prev;
+    
+    while (secondHalf) {
+        let temp1 = firstHalf.next;
+        let temp2 = secondHalf.next;
+        
+        firstHalf.next = secondHalf;
+        secondHalf.next = temp1;
+        firstHalf = temp1;
+        secondHalf = temp2;
+    }
+};
