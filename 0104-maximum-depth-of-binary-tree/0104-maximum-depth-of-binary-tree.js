38d478161e4544782e2125f0e4a12e0519835361
jshin720 <80377105+jshin720@users.noreply.github.com>
2024-01-25 12:28:53 -0500

    Time: 62 ms (48.66%), Space: 51.8 MB (5.61%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    // let counter = 0;
+    
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+// const dfs = (node) => {
+//     if (!node) return null;
+    
+    
+// }
